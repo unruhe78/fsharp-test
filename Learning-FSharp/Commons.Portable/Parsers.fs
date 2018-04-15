c39ee088e6631efd68a2ef41ee0548eaeb5c12d8
unruhe78 <matthiasunruhe@gmx.de>
2018-04-15 20:05:17 +0200

    Added function "bind" to Parser-Module

--- Learning-FSharp/Commons.Portable/Parsers.fs
+++ Learning-FSharp/Commons.Portable/Parsers.fs
@@ -2,24 +2,31 @@
 
 open Commons.Types
 
-type Parser<'a> = Parser of (char list -> OperationResult<'a * char list, ParseError>)
+type Parser<'TContent, 'TState> = Parser of ('TState -> OperationResult<'TContent * 'TState, ParseError>)
 and ParseError  =
 | DiscardPreviousSuccess
 | FollowUpError
 | InputCannotBeEmpty
-| InputMissmatch of (char * char)
+| InputMissmatch of (string * string)
     member e.Message =
         match e with
         | DiscardPreviousSuccess           -> sprintf "Parse operation failed due to a follow-up error!"
         | FollowUpError                    -> sprintf "Parse operation failed due to previous error!"
         | InputCannotBeEmpty               -> sprintf "Cannot parse input value because it's empty!"
-        | InputMissmatch (expected, value) -> sprintf "Parse operation failed! Expected value '%c' but found value '%c'!" expected value
+        | InputMissmatch (expected, value) -> sprintf "Parse operation failed! Expected value '%s' but found value '%s'!" expected value
 
 
 module Parser =
     let run parser input =
         let (Parser parserfunc) = parser
         input |> parserfunc
+
+    let map mapper parser =
+        let innerParser input =
+            match run parser input with
+            | Success (v, rest) -> Success (v |> mapper, rest)
+            | Error error -> Error error
+        Parser innerParser
 
     let private orParse parser1 parser2 =
         let innerParser input =
@@ -42,18 +49,20 @@ module Parser =
 
     let (&&&) = andParse
 
-    let map mapper parser =
-        let innerParser input =
-            match run parser input with
-            | Success (v, rest) -> Success (v |> mapper, rest)
-            | Error error -> Error error
-        Parser innerParser
-
-    let apply func param =
-        func &&& param
+    let apply func parser =
+        func &&& parser
         |> map (fun (f, x) -> f x)
 
     let (<*>) = apply
+
+    let bind func parser =
+         let innerParser input =
+            match run parser input with
+            | Success (content, state) -> run (content |> func) state
+            | Error error -> Error error
+         Parser innerParser
+
+    let (>>=) = bind
 
     let returnAsParser value =
         let innerParser input = 
@@ -69,12 +78,12 @@ module Parser =
         | parser::remainingParsers -> map2 (fun h t -> h::t) parser (processParserList remainingParsers)
 
     let expectChar expected =
-        let innerParser input =
+        let innerParser (input:char list) =
             match input with
             | []         -> Error ParseError.InputCannotBeEmpty
             | head::tail -> if head = expected 
                             then Success (head, tail) 
-                            else Error (ParseError.InputMissmatch (expected, head))
+                            else Error (ParseError.InputMissmatch (expected.ToString(), head.ToString()))
     
         Parser innerParser
     
